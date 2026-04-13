e82fa2ca61c46d978ce328dc30501d72c09a8d55
mirellanascimento86 <mirellanascimento86@gmail.com>
2026-04-12 22:37:53 -0300

    Delete api/carregar-servicos.js

--- api/carregar-servicos.js
+++
@@ -1,8 +0,0 @@
-// api/carregar-servicos.js
-export default async function handler(req, res) {
-  const r = await fetch(`${process.env.SUPABASE_URL}/rest/v1/servicos?select=*`, {
-    headers: { apikey: process.env.SUPABASE_KEY, Authorization: `Bearer ${process.env.SUPABASE_KEY}` }
-  });
-  return res.status(200).json(await r.json() || []);
-}
- 
